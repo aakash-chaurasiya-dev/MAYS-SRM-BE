5e59d7e7d23e8b58a42d4d66436432cc969fae78
rahullll0117 <y.rahulkumar012@gmail.com>
2026-07-26 16:33:13 +0530

    Role_changes3

--- src/main/java/com/mays/srm/organization/entities/Department.java
+++ src/main/java/com/mays/srm/organization/entities/Department.java
@@ -1,6 +1,14 @@
 package com.mays.srm.organization.entities;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -21,17 +29,6 @@ public class Department {
     @Column(name = "department_description")
     private String departmentDescription;
 
-    @jakarta.persistence.Column(name = "is_locked")
-    private Boolean isLocked = false;
-
-    @org.hibernate.annotations.CreationTimestamp
-    @jakarta.persistence.Column(name = "insert_date", updatable = false)
-    private java.util.Date insertDate;
-
-    @org.hibernate.annotations.UpdateTimestamp
-    @jakarta.persistence.Column(name = "last_update_date")
-    private java.util.Date lastUpdateDate;
-
     public Integer getDepartmentId() {
         return this.departmentId;
     }
@@ -48,11 +45,22 @@ public class Department {
         this.departmentName = departmentName;
     }
 
-    public String getDepartmentDescription() {
-        return this.departmentDescription;
+    @jakarta.persistence.Column(name = "is_locked")
+    private Boolean isLocked = false;
+
+    public Boolean getIsLocked() {
+        return this.isLocked;
     }
 
-    public void setDepartmentDescription(String departmentDescription) {
-        this.departmentDescription = departmentDescription;
+    public void setIsLocked(Boolean isLocked) {
+        this.isLocked = isLocked;
     }
+
+    @org.hibernate.annotations.CreationTimestamp
+    @jakarta.persistence.Column(name = "insert_date", updatable = false)
+    private java.util.Date insertDate;
+
+    @org.hibernate.annotations.UpdateTimestamp
+    @jakarta.persistence.Column(name = "last_update_date")
+    private java.util.Date lastUpdateDate;
 }
